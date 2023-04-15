472babd546853a013ce2e9287da33915c2b0df35
Cameron Delano <62901589+knowbase@users.noreply.github.com>
2023-04-14 20:49:27 -0700

    Update data.tf

--- arcadia/data.tf
+++ arcadia/data.tf
@@ -7,10 +7,12 @@ data "tfe_outputs" "eks" {
   workspace = "eks"
 }
 data "tfe_outputs" "nap" {
+  count = nap ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "nap"
 }
 data "tfe_outputs" "nic" {
+  count = nic ? 1 : 0
   organization = var.tf_cloud_organization
   workspace = "nic"
 }
