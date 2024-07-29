d0b9d202dc28b7841a753af83f7931ce8d20e526
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-07-29 23:06:57 +0530

    Update eks_cluster.tf

--- eks-cluster/eks_cluster.tf
+++ eks-cluster/eks_cluster.tf
@@ -57,11 +57,11 @@ resource "aws_eks_node_group" "private-node-group-2-tf" {
   node_role_arn  = aws_iam_role.workernodes.arn
   subnet_ids =  [for i in aws_subnet.eks-internal: i.id]
   ami_type       = "AL2_x86_64"
-  instance_types = ["t3.small"]
+  instance_types = ["t3.xlarge"]
  
   scaling_config {
-   desired_size = 2
-   max_size   = 3
+   desired_size = 1
+   max_size   = 1
    min_size   = 1
   }
 
