8794d767e9a1d9320da2f42708eef801a07bf3ec
Mohammed Janibasha <jani.basha.5000@gmail.com>
2024-08-02 10:01:16 +0530

    Update prometheus.tf

--- nap/prometheus.tf
+++ nap/prometheus.tf
@@ -2,7 +2,7 @@ resource "helm_release" "prometheus" {
     name = format("%s-pro-%s", local.project_prefix, local.build_suffix)
     repository = "https://prometheus-community.github.io/helm-charts"
     chart = "prometheus"
-    version = latest
+    #version = latest
     namespace = kubernetes_namespace.monitoring.metadata[0].name
     values = [file("./charts/prometheus/values.yaml")]
 }
